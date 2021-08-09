84ba10a7fa8725f4b50b4b89351f7368064ecb0a
JaithunMahira <jaithunmahira@gmail.com>
2021-08-09 09:05:39 +0530

    Differences in Jsx and HTML elements

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 //Create a React Component
 const App = () => {
-    return <div > Hi there! </div>
+
+    //diff in jsx and html 1. styling prop 2. className 3. referencing Js variables 4. htmlFor
+    const labelText = "Enter Name :";
+    const buttonText = {text : "Click Me!"};
+    return (
+        <div > 
+            <label className="label" htmlFor="name" > {labelText} </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor : 'blue', color : 'white'}}> {buttonText.text} </button>
+        </div>
+    );
 }
 
 //Take the react Component and show it in screen
