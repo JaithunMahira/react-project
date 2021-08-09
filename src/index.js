f17ff32fd74b9ac2a857cfb4061d48e4d8f47130
JaithunMahira <jaithunmahira@gmail.com>
2021-08-09 08:28:16 +0530

    index.js file components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
+//Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create a React Component
+const App = () => {
+    return <div > Hi there! </div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Take the react Component and show it in screen
+ReactDOM.render(<App />, document.querySelector('#root'));
