a8e33b67c52793b5acd58af849dbba2fbb14643c
JaithunMahira <jaithunmahira@gmail.com>
2021-08-09 14:40:53 +0530

    Components, props and nested components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,62 @@
 //Import React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 //Create a React Component
 const App = () => {
+  //diff in jsx and html 1. styling prop 2. className 3. referencing Js variables 4. htmlFor
+  const labelText = "Enter Name :";
+  const buttonText = { text: "Click Me!" };
+  return (
+    <body>
+      {/* Section 1 and 2*/}
+      <div>
+        <label className="label" htmlFor="name">
+          {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: "blue", color: "white" }}>
+          {buttonText.text}
+        </button>
+      </div>
 
-    //diff in jsx and html 1. styling prop 2. className 3. referencing Js variables 4. htmlFor
-    const labelText = "Enter Name :";
-    const buttonText = {text : "Click Me!"};
-    return (
-        <div > 
-            <label className="label" htmlFor="name" > {labelText} </label>
-            <input id="name" type="text" />
-            <button style={{backgroundColor : 'blue', color : 'white'}}> {buttonText.text} </button>
-        </div>
-    );
-}
+      {/*Section 3 - loading comments 1. Added faker to load data 2. Added
+      semantic.min.css for css styling 3. Added CommentDetail.js/ Components
+      Nesting */}
+      <div className="ui container comments">
+        <ApprovalCard>
+          <CommentDetail
+            author="Jaithun"
+            profilePic={faker.image.avatar()}
+            timeAgo="Today at 3:00 am"
+            comment="Good!"
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail
+            author="Jumana"
+            profilePic={faker.image.avatar()}
+            timeAgo="Today at 6:00 am"
+            comment="Nice Post!"
+          />
+        </ApprovalCard>
+
+        <ApprovalCard>
+          <CommentDetail
+            author="Rahman"
+            profilePic={faker.image.avatar()}
+            timeAgo="Today at 9:00 pm"
+            comment="Excellent!"
+          />
+        </ApprovalCard>
+      </div>
+    </body>
+  );
+};
 
 //Take the react Component and show it in screen
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
